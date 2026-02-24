14a2ea151d3b80a4d6690ed7930156c2fa09dc0d
Venkata Reddy Bommu <vtu25671@veltech.edu.in>
2026-02-24 09:46:37 +0530

    Time: 7 ms (95.76%) | Memory: 70.4 MB (8.78%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,26 +1,17 @@
 import java.util.Random;
-
 class Solution {
-
     Random rand = new Random();
-
     public int findKthLargest(int[] nums, int k) {
         int target = nums.length - k;
         return quickSelect(nums, 0, nums.length - 1, target);
     }
-
     private int quickSelect(int[] nums, int left, int right, int k) {
-
         while (left <= right) {
-
             int pivotIndex = left + rand.nextInt(right - left + 1);
             int pivot = nums[pivotIndex];
-
-            // 3-way partition
-            int lt = left;     // < pivot
-            int gt = right;    // > pivot
+            int lt = left;
+            int gt = right;
             int i = left;
-
             while (i <= gt) {
                 if (nums[i] < pivot) {
                     swap(nums, lt++, i++);
@@ -30,24 +21,16 @@ class Solution {
                     i++;
                 }
             }
-
-            // Now:
-            // left → lt-1 = smaller
-            // lt → gt = equal
-            // gt+1 → right = greater
-
             if (k < lt) {
                 right = lt - 1;
             } else if (k > gt) {
                 left = gt + 1;
             } else {
-                return nums[k];   // inside equal zone
+                return nums[k];
             }
         }
-
         return -1;
     }
-
     private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
