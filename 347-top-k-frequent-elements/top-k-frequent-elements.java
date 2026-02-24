a24b06724fe6706269e53447243168d6030b6ba2
Venkata Reddy Bommu <vtu25671@veltech.edu.in>
2026-02-24 09:51:00 +0530

    Time: 16 ms (38.93%) | Memory: 47.3 MB (97.65%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        PriorityQueue<Integer> minHeap =
+                new PriorityQueue<>((a, b) -> map.get(a) - map.get(b));
+        for (int key : map.keySet()) {
+            minHeap.add(key);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        int[] result = new int[k];
+        for (int i = k - 1; i >= 0; i--) {
+            result[i] = minHeap.poll();
+        }
+        return result;
+    }
+}
